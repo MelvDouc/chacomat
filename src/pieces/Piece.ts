cc15c1ecd6178f9d1f83fe80b072271a05808c9d
MelvDouc <melv.douc@gmail.com>
2023-01-10 23:26:23 +0100

    Using TS access mods

--- src/pieces/Piece.ts
+++ src/pieces/Piece.ts
@@ -18,26 +18,30 @@ import castlingCoords from "./castling.js";
 import pseudoLegalPawnMoves from "./pawn-moves.js";
 
 export default class Piece {
-  static readonly Types = PieceType;
+  public static readonly Types = PieceType;
+  public static readonly castlingCoords = castlingCoords;
+  private static readonly slidingTypes = [
+    PieceType.ROOK,
+    PieceType.BISHOP,
+    PieceType.QUEEN,
+  ];
 
-  static readonly INITIAL_PIECE_RANKS: BlackAndWhite<ChessRank> = {
+  public static readonly INITIAL_PIECE_RANKS: BlackAndWhite<ChessRank> = {
     [Color.WHITE]: 7,
     [Color.BLACK]: 0,
   };
 
-  static readonly CASTLED_ROOK_FILES = {
+  public static readonly CASTLED_ROOK_FILES = {
     [Wing.QUEEN_SIDE]: 3,
     [Wing.KING_SIDE]: 5,
   };
 
-  static readonly MIDDLE_RANKS: BlackAndWhite<ChessRank> = {
+  public static readonly MIDDLE_RANKS: BlackAndWhite<ChessRank> = {
     [Color.WHITE]: 4,
     [Color.BLACK]: 3,
   };
 
-  static readonly castlingCoords = castlingCoords;
-
-  static fromInitial(initial: PieceInitial) {
+  public static fromInitial(initial: PieceInitial) {
     const initialKey = initial.toUpperCase() as WhitePieceInitial;
     return new Piece(
       (initialKey === initial) ? Color.WHITE : Color.BLACK,
@@ -45,79 +49,66 @@ export default class Piece {
     );
   }
 
-  static promote(piece: Piece, newType: Promotable): Piece {
+  public static promote(piece: Piece, newType: Promotable): Piece {
     piece.type = PieceByInitial[newType] as unknown as PieceType;
     return piece;
   }
 
-  static #isSafe(x: number, y: number): boolean {
-    return x >= 0 && x < 8 && y >= 0 && y < 8;
-  }
-
-  readonly color: Color;
-  type: PieceType;
+  public readonly color: Color;
+  public type: PieceType;
 
   constructor(color: Color, type: PieceType) {
     this.color = color;
     this.type = type;
   }
 
-  get initial(): PieceInitial {
+  public get initial(): PieceInitial {
     return (this.color === Color.WHITE)
       ? PieceByInitial[this.type] as WhitePieceInitial
       : (PieceByInitial[this.type] as WhitePieceInitial)
         .toLowerCase() as BlackPieceInitial;
   }
 
-  get oppositeColor(): Color {
+  public get oppositeColor(): Color {
     return -this.color as Color;
   }
 
-  get oppositeMidRank(): ChessRank {
+  public get oppositeMidRank(): ChessRank {
     return Piece.MIDDLE_RANKS[this.oppositeColor];
   }
 
-  get #offsets(): { x: number[]; y: number[]; } {
+  private get offsets(): { x: number[]; y: number[]; } {
     return offsets[this.color][this.type];
   }
 
-  *#slidingAttackedCoords(
-    srcCoords: Coords,
-    board: Board,
-  ): CoordsGenerator {
-    for (let i = 0; i < this.#offsets.x.length; i++) {
-      const coords = {
-        x: srcCoords.x + this.#offsets.x[i],
-        y: srcCoords.y + this.#offsets.y[i]
-      };
-      while (Piece.#isSafe(coords.x, coords.y)) {
-        yield { ...coords };
-        if (board.get(coords)) break;
-        coords.x += this.#offsets.x[i];
-        coords.y += this.#offsets.y[i];
+  private *slidingAttackedCoords(srcCoords: Coords, board: Board): CoordsGenerator {
+    for (let i = 0; i < this.offsets.x.length; i++) {
+      let x = srcCoords.x + this.offsets.x[i],
+        y = srcCoords.y + this.offsets.y[i];
+      while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+        yield { x, y };
+        if (board.get({ x, y })) break;
+        x += this.offsets.x[i];
+        y += this.offsets.y[i];
       }
     }
   }
 
-  *attackedCoords(srcCoords: Coords, board: Board): CoordsGenerator {
-    if (
-      this.type === PieceType.BISHOP || this.type === PieceType.ROOK ||
-      this.type === PieceType.QUEEN
-    ) {
-      yield* this.#slidingAttackedCoords(srcCoords, board);
+  public *attackedCoords(srcCoords: Coords, board: Board): CoordsGenerator {
+    if (Piece.slidingTypes.includes(this.type)) {
+      yield* this.slidingAttackedCoords(srcCoords, board);
       return;
     }
 
-    for (let i = 0; i < this.#offsets.x.length; i++) {
-      const x = srcCoords.x + this.#offsets.x[i],
-        y = srcCoords.y + this.#offsets.y[i];
-      if (Piece.#isSafe(x, y)) {
+    for (let i = 0; i < this.offsets.x.length; i++) {
+      const x = srcCoords.x + this.offsets.x[i],
+        y = srcCoords.y + this.offsets.y[i];
+      if (x >= 0 && x < 8 && y >= 0 && y < 8)
         yield { x, y };
-      }
     }
   }
 
-  *pseudoLegalMoves(srcCoords: Coords, position: Position) {
+  public *pseudoLegalMoves(srcCoords: Coords, position: Position): CoordsGenerator {
     if (this.type === PieceType.PAWN) {
       yield* pseudoLegalPawnMoves(this, srcCoords, position);
       return;
@@ -128,7 +119,7 @@ export default class Piece {
         yield targetCoords;
   }
 
-  clone(): Piece {
+  public clone(): Piece {
     return new Piece(this.color, this.type);
   }
 }
