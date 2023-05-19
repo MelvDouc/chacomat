da434a6f5921be2fde0b960329198118c84c0eab
MelvDouc <melv.douc@gmail.com>
2023-05-19 19:51:24 +0200

    misc

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- [ ] Parse game result
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,17 +1,20 @@
 import Board from "@src/game/Board.js";
+import parseVariations from "@src/pgn-fen/parse-variations.js";
 import ChessGame from "@src/game/ChessGame.js";
 
 const board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 board.log();
 
-const game = new ChessGame({
-  pgn: `
-    1.e4 e5
-      ( 1...c5 2.f4
-        ( 2.Nc3 )
-      2...e6 )
-      ( 1...c6 )
-    2.Nf3 Nc6 3.Bb5 a6 *
-  `
-});
-console.log(game.toString());
+const pgn = `
+  1.e4 e5
+    ( 1...c5 2.f4
+      ( 2.Nc3 )
+    2...e6 )
+    ( 1...c6 )
+  2.Nf3 Nc6 3.Bb5 a6 *
+`;
+const game = new ChessGame({ pgn });
+console.log(
+  parseVariations(pgn)
+);
+game.currentPosition.board.log();
