543879dfb41b037e1d175d1bc2995ce996e27af3
MelvDouc <melv.douc@gmail.com>
2023-12-20 18:42:26 +0100

    Fix type exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,28 @@ export { default as SquareIndex } from "$src/constants/SquareIndex.ts";
 export { default as ChessGame } from "$src/game/ChessGame.ts";
 export { default as globalConfig } from "$src/global-config.ts";
 export { default as Pieces } from "$src/pieces/Pieces.ts";
-export type * as ChacoMat from "$src/typings/types.public.ts";
 export { GameResults } from "pgnify";
+
+export type {
+  BlackPieceInitial,
+  Board,
+  CastlingRights,
+  Color,
+  ColorName,
+  GameResult,
+  JSONBoard,
+  JSONCastlingRights,
+  JSONPiece,
+  JSONPosition,
+  Move,
+  NAG,
+  NullMove,
+  PGNHeaders,
+  Piece,
+  PieceInitial,
+  Point,
+  Position,
+  RealMove,
+  WhitePieceInitial,
+  Wing
+} from "$src/typings/types.ts";
